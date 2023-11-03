ca1f7344e4b0563e68694ef0be8c0e51a9620565
beatrizzcoan <136799922+beatrizzcoan@users.noreply.github.com>
2023-11-03 06:47:54 -0300

    atividadePOO

---
+++ fixacao/Produto.java
@@ -0,0 +1,22 @@
+package atividadePOOnota;
+
+public class Produto {
+	
+	int id;
+	String nome;
+	double preco;
+	
+	public Produto() {
+		
+	}
+	
+	public Produto(int id, String nome,double preco) {
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+	
+	public void mostrar() {
+		System.out.println(" | " + id + "    |" + nome + "RS" + preco);
+	}
+}
